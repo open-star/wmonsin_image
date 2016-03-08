3834d359368a9d4ed98010d95b06c7ce4c255b33
oda <oda.mikio@gmail.com>
2016-03-08 21:31:04 +0900

    ...

--- wmonsin/app.js
+++ wmonsin/app.js
@@ -60,7 +60,7 @@ app.set('view engine', 'jade');
 alert_log({}, 'Jade Start.');
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(bodyParser({ limit: '50mb' }));
+//app.use(bodyParser({limit: '50mb'}));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
@@ -77,9 +77,9 @@ process.on('uncaughtException', function (err) {
 process.on('exit', function (code) {
     logger.info("exit " + code);
 });
-process.on('SIGINT', function () {
-    logger.info("SIGINT");
-});
+//process.on('SIGINT', function () {
+//    logger.info("SIGINT");
+//});
 app.use(session({
     name: config.sessionName,
     secret: config.sessionkey,
@@ -102,12 +102,11 @@ app.use(passport.initialize());
 app.use(passport.session());
 //passport
 if (config.state === 'development') {
-    app.use(morgan({ format: 'original', immediate: true }));
+    app.use(morgan('original', { immediate: true }));
 }
 else {
     var rotatestream = require('logrotate-stream');
-    app.use(morgan({
-        format: 'combined',
+    app.use(morgan('combined', {
         stream: rotatestream({ file: __dirname + '/logs/access.log', size: '100k', keep: 3 })
     }));
 }
